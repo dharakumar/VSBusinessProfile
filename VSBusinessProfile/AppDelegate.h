88eaef964dc8d5a4b72877688a86c04b2dbceb9e
Admin <admin@Admins-MacBook-Pro-3.local>
2016-12-20 11:20:58 -0800

    Initial Commit

---
+++ VSBusinessProfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSBusinessProfile
+//
+//  Created by Admin on 12/20/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSBusinessProfile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSBusinessProfile
+//
+//  Created by Admin on 12/20/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
